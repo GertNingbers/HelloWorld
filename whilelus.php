e29f771fff283729605983a78b3ac9ac9608aafd
GertNingbers <97081115@st.deltion.nl>
2024-09-17 10:46:27 +0200

    v2

---
+++ whilelus.php
@@ -0,0 +1,15 @@
+<?php
+
+$teller=1;
+$maximum=15;
+
+echo "Teller is voor de while lus".$teller."\n";
+
+while ( $teller <= $maximum ) {
+    echo "teller is nu ".$teller."\n";
+    $teller++;
+}
+
+echo "teller is na de while lus".$teller."\n";
+
+?>
