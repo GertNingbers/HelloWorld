07f80998647b6cdc996a7a229720801c0b759b37
GertNingbers <97081115@st.deltion.nl>
2024-09-17 08:41:34 +0200

    v8

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,12 +1,13 @@
 <?php
 
 $tekst="Hello,World!\n";
+$tekst9999999="Bob,says!\n";
 
-echo $tekst;
+echo $tekst9999999;
 echo $tekst;
 echo $tekst;
 echo $tekst;
 echo $tekst; 
-
+echo $tekst;
 
  ?>
