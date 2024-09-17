4657d44ba6c918b80e87fb4fccbe46fe235f808b
GertNingbers <97081115@st.deltion.nl>
2024-09-17 09:57:21 +0200

    v2

--- invoer.php
+++ invoer.php
@@ -1,7 +1,27 @@
 <?php
 
-$invoer = readline("Geef een woord : ");
-echo "jij gaf het volgende woord: $invoer";
+$invoer = readline("jouw voornaam is?: ");
+echo "\n";
+
+$invoer1 = readline("jouw achternaam?: ");
+echo "\n";
+
+$invoer2 = readline("en hoe oud ben je?: ");
+echo "\n";
+
+$invoer3 = readline("waar woon je?: ");
+echo "\n";
+
+$invoer4 = readline("wat zijn jou hobby's?: ");
+echo "\n";
+
+echo "hallo mijn naam is: $invoer $invoer1";
+echo "\n";
+echo "ik ben $invoer2 jaar oud";
+echo "\n";
+echo "ik woon in $invoer3";
+echo "\n";
+echo "en mijn hobby's zijn $invoer4";
 echo "\n";
 
 ?>
