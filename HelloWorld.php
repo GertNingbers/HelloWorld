c076d318ed1d0bd73f63882ddeecda463c0e3515
GertNingbers <97081115@st.deltion.nl>
2024-09-17 11:02:18 +0200

    v1 with while lus

--- HelloWorld.php
+++ HelloWorld.php
@@ -2,12 +2,21 @@
 
 $tekst="Hello,World!\n";
 $tekst9999999="Bob,says!\n";
+$teller=1;
+$maximum=10;
 
 echo $tekst9999999;
 echo $tekst;
 echo $tekst;
 echo $tekst;
 echo $tekst; 
-echo $tekst;
+echo $tekst9999999;
+
+while ( $teller <= $maximum ) {
+    echo $tekst;
+    $teller++;
+}
+
+echo "Shut up Bob!!!"
 
  ?>
