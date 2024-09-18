4f66663ab5fccc64705724eb9de25b666ad15a7b
GertNingbers <97081115@st.deltion.nl>
2024-09-18 08:46:12 +0200

    v1 tafel

---
+++ tafel.php
@@ -0,0 +1,13 @@
+<?php
+
+$tafel=readline("welke tafel wil je zien? ");
+$begin=1;
+$eind=10;
+
+echo "\n\nTafel van $tafel:\n";
+
+for ($teller=$begin;$teller<=$eind;$teller++) {
+  $product=$teller*$tafel;
+  echo $teller." x ".$tafel." = ".$product."\n";  
+}
+?>
